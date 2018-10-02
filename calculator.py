0d7fdf40c51974452a8c159d2f4b579561c52b16
andrey <andrey.spb.ykubenko@example.com>
2018-10-02 19:19:24 +0300

    First commit

---
+++ calculator.py
@@ -0,0 +1,24 @@
+# Программа калькулятор. Выполняет арифметические действия над двумя числами, полученными от 
+# пользователя. Поддерживает действия: +-*/. Выход из программы производится вводом команды [exit]. 
+print("exit завершить работу программы")
+while True:
+	s = input ("знак(+,-,*,/):")
+	if s in('+','-','*','/'):
+		x = float (input ("x="))
+		y = float (input ("y="))
+		if s == '+':
+			print("%.2f"% (x + y))
+		elif s =='-':
+			print("%.2f"% (x - y))
+		elif s =='*':
+			print("%.2f"% (x*y))
+		elif s =='/':
+			if y == 0:
+				print("деление на ноль !")
+			else:
+				print("%.2f"% (x/y))
+		elif s == '0':
+		    print(input("exit"))
+with contextlib.closing(Closeable()):
+	sys.exit()
+

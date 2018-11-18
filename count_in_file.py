a1d8198d5f9dc84c1738ed0c541a052afa54cb43
andrey <andrey.spb.ykubenko@example.com>
2018-11-18 22:40:04 +0300

    пытаюсь

--- count_in_file.py
+++ count_in_file.py
@@ -12,14 +12,50 @@
 #программа должна вывести результат суммирования: 46
 #3. <продолжение следует>
 
+import sys
+
+if __name__ == "__main__":
+    if len (sys.argv) == 1:
+        print ("Привет, мир!")
+    else:
+        if len (sys.argv) < 3:
+            print ("Ошибка. Слишком мало параметров.")
+            sys.exit (1)
+
+        if len (sys.argv) > 3:
+            print ("Ошибка. Слишком много параметров.")
+            sys.exit (1)
+
+        param_name = sys.argv[1]
+        param_value = sys.argv[2]
+
+        if (param_name == "--name" or
+                param_name == "-n"):
+            print ("Привет, {}!".format (param_value) )
+        else:
+            print ("Ошибка. Неизвестный параметр '{}'".format (param_name) )
+            sys.exit (1)
+import os
+
+def file_base_name(file_name):
+    if '.' in file_name:
+        separator_index = file_name.index('.')
+        base_name = file_name[:separator_index]
+        return base_name
+    else:
+        return file_name
+
+def path_base_name(path):
+    file_name = os.path.basename(path)
+    return file_base_name(file_name)
 print ("python count_in_file.py, file,option")
 while True:
     file="option"
-    if option== "option" :
+    if option== "file" :
         print ("1. file_name")
         print ("2. option_name")
-	  elif option != ("option"): 
-	    print()
+	    elif option != 'option':
+	        print()
 option_name =("option")
 верно
 print("file_name,option_name")
